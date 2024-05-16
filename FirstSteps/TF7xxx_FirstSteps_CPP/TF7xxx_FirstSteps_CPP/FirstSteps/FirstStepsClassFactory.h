418b1c263bfc234879292a90c0b25023ed4368bb
John Helfrich <john.j.helfrich@ieee.org>
2024-05-16 11:44:47 -0400

    Adding First Steps Project

---
+++ FirstSteps/TF7xxx_FirstSteps_CPP/TF7xxx_FirstSteps_CPP/FirstSteps/FirstStepsClassFactory.h
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+// FirstSteps.h
+
+#pragma once
+
+#include "ObjClassFactory.h"
+
+class CFirstStepsClassFactory : public CObjClassFactory
+{
+public:
+	CFirstStepsClassFactory();
+	DECLARE_CLASS_MAP()
+};
+
+
---
+++ FirstSteps/TF7xxx_FirstSteps_CPP/TF7xxx_FirstSteps_CPP/FirstSteps/_products/TwinCAT RT (x64)/Release/FirstSteps/FirstStepsVersion.h
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////
+// FirstStepsVersion.h
+
+#ifndef FirstStepsVersion_H_INCLUDED
+#define FirstStepsVersion_H_INCLUDED
+
+#define FirstSteps_Version   0
+#define FirstSteps_Revision  0
+#define FirstSteps_Build     1
+
+#define FirstSteps_FileVersionMajor    0
+#define FirstSteps_FileVersionMinor    0
+#define FirstSteps_FileVersionBuild    0
+#define FirstSteps_FileVersionRevision 1
+#define FirstSteps_FileVersionStr      "0.0.0.1"
+#define FirstSteps_FileVersionRcStr    "0,0,0,1"
+
+#define FirstSteps_ProductMajor        3
+#define FirstSteps_ProductMinor        1
+#define FirstSteps_ProductBuild        4026
+#define FirstSteps_ProductRevision     4
+#define FirstSteps_ProductVersionStr   "3.1.4026.4"
+#define FirstSteps_ProductVersionRcStr "3,1,4026,4"
+
+#define FirstSteps_CompanyNameStr      "C++ Module Vendor"
+#define FirstSteps_InternalNameStr     "FirstSteps"
+#define FirstSteps_LibraryIdNameStr    "C++ Module Vendor|FirstSteps|0.0.0.1"
+
+#ifndef RC_INVOKED
+static const GUID VID_FirstSteps      = { 0xd3374372, 0x8d08, 0x901d, { 0x29, 0x05, 0xad, 0xe3, 0x3c, 0xd6, 0x39, 0x79 } };
+#endif
+
+#endif // include guard
